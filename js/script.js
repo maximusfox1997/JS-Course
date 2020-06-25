ea251409dbf98e1936e608b8b288f79e602e4d9f
Maxim <maxim.malikov1997@gmail.com>
2020-06-25 07:01:17 +0000

    Practise part 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt("How much films you have watched yet", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastWatchedFilmFirst = prompt("One of the most recently watched films ?", "");
+const filmRateFirst = +prompt("How much do you rate it ?", "");
+const lastWatchedFilmSecond = prompt("One of the most recently watched films ?", "");
+const filmRateSecond = +prompt("How much do you rate it ?", "");
+
+personalMovieDB.movies[lastWatchedFilmFirst] = filmRateFirst;
+personalMovieDB.movies[lastWatchedFilmSecond] = filmRateSecond;
+
+console.log(personalMovieDB);
