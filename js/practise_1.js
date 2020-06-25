a6a5e634bb29c1a02b8a9744828976b24e7c013e
Maxim <maxim.malikov1997@gmail.com>
2020-06-25 14:01:05 +0000

    Practise part 2

--- js/practise_1.js
+++ js/practise_1.js
@@ -10,12 +10,50 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastWatchedFilmFirst = prompt("One of the most recently watched films ?", "");
-const filmRateFirst = +prompt("How much do you rate it ?", "");
-const lastWatchedFilmSecond = prompt("One of the most recently watched films ?", "");
-const filmRateSecond = +prompt("How much do you rate it ?", "");
+for(let i = 0; i < 2; i++){
+    const lastWatchedFilm = prompt("One of the most recently watched films ?", "");
+    const filmRate = prompt("How much do you rate it ?", "");
 
-personalMovieDB.movies[lastWatchedFilmFirst] = filmRateFirst;
-personalMovieDB.movies[lastWatchedFilmSecond] = filmRateSecond;
+    if(lastWatchedFilm !== null && filmRate !== null && 
+        lastWatchedFilm !== '' && filmRate !== '' && 
+        lastWatchedFilm.length < 50){
+
+        personalMovieDB.movies[lastWatchedFilm] = filmRate;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+
+}
+
+// let iterator = 0;
+// while(iterator < 2){
+//     const lastWatchedFilm = prompt("One of the most recently watched films ?", "");
+//     const filmRate = prompt("How much do you rate it ?", "");
+
+//     if(lastWatchedFilm !== null && filmRate !== null && 
+//         lastWatchedFilm !== '' && filmRate !== '' && 
+//         lastWatchedFilm.length < 50){
+
+//         personalMovieDB.movies[lastWatchedFilm] = filmRate;
+//         console.log("Done");
+//     } else {
+//         console.log("Error");
+//         iterator--;
+//     }
+
+//     iterator++;
+// }
+
+if (personalMovieDB.count < 10) {
+    console.log("It's too few movies watched");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("You're classical watcher");
+} else if (personalMovieDB.count >= 50) {
+    console.log("You are a movie fan");
+} else {
+    console.log("Error");
+}
 
 console.log(personalMovieDB);
